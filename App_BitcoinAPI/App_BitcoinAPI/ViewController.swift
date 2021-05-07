24d0345517158b26245231ceac45044813590599
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-05-06 21:01:03 -0300

    First Commit

---
+++ App_BitcoinAPI/App_BitcoinAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App_BitcoinAPI
+//
+//  Created by Kaique Lopes on 06/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "")
+        let data = URLSession.shared.dataTask(with: <#T##URLRequest#>, completionHandler: <#T##(Data?, URLResponse?, Error?) -> Void#>)
+    }
+
+
+}
+
